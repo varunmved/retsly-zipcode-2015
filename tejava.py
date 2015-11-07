b6c2b03582d06fa1df2f36ec5402bbe416fcc15c
Cha Li <chavli@gmail.com>
2015-11-07 14:20:07 -0800

    new endpoint returns station info

--- tejava.py
+++ tejava.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify, g
 from flask import request, Response, redirect
 
+import endpoints.caltrain_api as caltrain_api
+
 app = Flask(__name__)
+app.register_blueprint(caltrain_api.api_blueprint, url_prefix="/caltrain")
+
 
 @app.route("/helloworld")
 def helloworld():
