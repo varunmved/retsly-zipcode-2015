7a14fd54ce08882c4d272d38220b05d57fe7238e
Cha Li <chavli@gmail.com>
2015-11-08 06:07:25 -0800

    using bulk feature of distance matrix

--- utils/travelpolygon.py
+++ utils/travelpolygon.py
@@ -2,6 +2,7 @@ import os
 import math as math
 import googlemaps
 import datetime as datetime
+import pdb
 
 
 token = os.getenv("GOOGLE_TOKEN", "notatoken")
@@ -27,7 +28,7 @@ class Point:
     def update(self):
         self.long = self.long + (self.r * math.cos(self.theta))
         self.lat = self.lat + (self.r * math.sin(self.theta))
-        
+
     def __str__(self):
         a = "lat: %s, long: %s, r: %s, theta: %s" % (self.lat, self.long, self.r, self.theta)
         return a
@@ -44,13 +45,13 @@ class TravelPolygon:
         for theta in range(0, 360, 360/slices):
             point = Point(r_init, theta, longitude,latitude)
             self.points.append(point)
-  
+
     def __str__(self):
         s = ""
         for point in self.points:
             s += "%s, %s\n" % (point.lat, point.long)
         return s
-    
+
     def vertices(self):
         vertices = []
         for point in self.points:
@@ -62,60 +63,79 @@ class TravelPolygon:
         alldone = False
         limit = 20
         i = 0
+        tmode = "bicycling"
 
-        depart = datetime.datetime(2015,11,10,7,0,0) 
+        depart = datetime.datetime(2015,11,10,7,0,0)
         while not alldone and i < limit:
             alldone = True
             i += 1
-            # print("------ iteration: %s" % i)
+            print("------ iteration: %s" % i)
             try:
-                for point in self.points:
-                    # print(point)
-                    if not point.done:
-                        pointLatLong = str(point.lat) + ','+ str(point.long)
-                        startLatLong = str(self.centerLat)+ ',' + str(self.centerLong)
-                        response = gmaps.distance_matrix(startLatLong,pointLatLong
-                            , mode='driving'
-                            , departure_time=depart)
-                        
-                        # print(response["rows"][0]["elements"][0])
-                        # we went too far with this point, so update it with it's previous position
-                        if response["rows"][0]["elements"][0]["status"] == "ZERO_RESULTS":
-                            point.r = point.prev_r
-                            point.update()
-                            point.done = True   
-                            continue
+                # go through all the points and make 1 distance matrix request for all of
+                # them
+                startLatLong = str(self.centerLat)+ ',' + str(self.centerLong)
+                polyLatLongs = "|".join([",".join([str(point.lat), str(point.long)]) for point in self.points])
 
-                        travelTime = response['rows'][0]['elements'][0]['duration_in_traffic']['value']
-                        
-                        # how far off the point is from the desired distance. we'll accept 10% error
-                        # print("max time: %s, travel_time: %s" % (self.max_time, travelTime))
-                        dist_error = (1.0 * abs(self.max_time - travelTime))/self.max_time 
-                        # print("theta: %s, %s -- %s, %s" % (point.theta, self.max_time, travelTime, dist_error))
+                # start points, dest point
+                response = gmaps.distance_matrix(polyLatLongs, startLatLong
+                        , mode = tmode
+                        , departure_time = depart)
 
-                        if (dist_error <= .4 and travelTime < self.max_time) or\
-                                dist_error <= .15:
-                            point.done = True
-                        elif (travelTime > self.max_time):
-                            point.upper_r = point.r
-                            self.prev_r = point.r
-                            point.r = (point.r + point.lower_r) / 2.0 
-                            point.update()
-                        elif(travelTime < self.max_time):
-                            point.lower_r = point.r
-                            point.prev_r = point.r
-                            if point.upper_r is None:
-                                point.r +=  (3/69.0) # add 10 miles ish
-                                # point.r *= 2 
+                if "status" in response and response["status"] == "OK":
+                    # all distances are in the rows key, listed in the order the cordinates were given
+                    for j in range(0, len(response["rows"])):
+                        result = response["rows"][j]
+                        point = self.points[j]
+
+                        if not point.done:
+                            if result["elements"][0]["status"] == "ZERO_RESULTS":
+                                point.r = point.prev_r
+                                point.update()
+                                point.done = True
+                                continue
+                            
+                            if tmode == "driving":
+                                travelTime = result['elements'][0]['duration_in_traffic']['value']
                             else:
-                                point.r = (point.r + point.upper_r) / 2.0
-                            point.update()
+                                travelTime = result['elements'][0]['duration']['value']
 
-                    alldone = alldone and point.done
+                            # how far off the point is from the desired distance. we'll accept 10% error
+                            # print("max time: %s, travel_time: %s" % (self.max_time, travelTime))
+                            dist_error = (1.0 * abs(self.max_time - travelTime))/self.max_time
+                            # print("theta: %s, %s -- %s, %s" % (point.theta, self.max_time, travelTime, dist_error))
+
+                            if (dist_error <= .4 and travelTime < self.max_time) or\
+                                    dist_error <= .15:
+                                point.done = True
+                            elif (travelTime > self.max_time):
+                                point.upper_r = point.r
+                                self.prev_r = point.r
+                                point.r = (point.r ) / 2.0
+                                point.update()
+                            elif(travelTime < self.max_time):
+                                point.lower_r = point.r
+                                point.prev_r = point.r
+                                if point.upper_r is None:
+                                    # point.r +=  (2/69.0) # add 10 miles ish
+                                    point.r *= 1.25
+                                else:
+                                    point.r = (point.r + point.upper_r) / 2.0
+                                point.update()
+
+                        alldone = alldone and point.done
+                else:
+                    print("distance matrix failed")
+                    pdb.set_trace()
+                    break
+
+
             except Exception as e:
                 print(e)
+                pdb.set_trace()
 
+# Lumo Offices: 37.4256015,-122.1459576
+# zip.Code: 37.788894,-122.4002976
 
-# shape = TravelPolygon(37.4256015,-122.1459576, 45, 16)
+# shape = TravelPolygon(37.788894,-122.4002976, 30, 16)
 # shape.fit()
 # print(shape)
