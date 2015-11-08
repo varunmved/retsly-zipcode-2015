a18d78b138a411a2311c2a56308f504cd0ce5fe3
Cha Li <chavli@gmail.com>
2015-11-08 00:26:06 -0800

    new endpoint

--- endpoints/tejava_api.py
+++ endpoints/tejava_api.py
@@ -8,10 +8,21 @@ api_blueprint = Blueprint("tejava_api", __name__)
 @api_blueprint.route("/testresults")
 def test_results():
     code = 200
-    polygon = buildpolygon(0, 0, 10)
+    polygon = buildpolygon(37.788889, -122.399864, 10, 4)
     results = polylistings(polygon)
     
     if "error" in results:
         code = 500
     
     return jsonify(results), code
+
+@api_blueprint.route("/polylistings/<float:latitude>/<float:longitude>/<int:max_minutes>")
+def listings(latitude, longitude, max_minutes):
+    code = 200
+    # TODO: ignore endpoint params right now
+    polygon = buildpolygon(37.788889, -122.399864, 4)
+    results = polylistings(polygon)
+
+    if "error" in results: code = 500
+    return jsonify(results), code
+
